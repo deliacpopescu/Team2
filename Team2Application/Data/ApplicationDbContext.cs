0a9ae50f9ca375ec633be52418393f09e9cadc2c
Rares Tant <79656115+tant-rares-30127@users.noreply.github.com>
2021-08-10 12:10:50 +0300

    Added controllers

--- Team2Application/Data/ApplicationDbContext.cs
+++ Team2Application/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Team2Application.Models;
 
 namespace Team2Application.Data
 {
@@ -12,5 +13,8 @@ namespace Team2Application.Data
             : base(options)
         {
         }
+        public DbSet<Team2Application.Models.Intern> Intern { get; set; }
+        public DbSet<Team2Application.Models.Skill> Skill { get; set; }
+        public DbSet<Team2Application.Models.LibraryResource> LibraryResource { get; set; }
     }
 }
