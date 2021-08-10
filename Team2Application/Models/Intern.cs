119cc2292b8676962538225cb311a8178f37f6e3
Rares Tant <79656115+tant-rares-30127@users.noreply.github.com>
2021-08-10 11:59:54 +0300

    Models only

---
+++ Team2Application/Models/Intern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team2Application.Models
+{
+    public class Intern
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ Team2Application/Models/LibraryResource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team2Application.Models
+{
+    public class LibraryResource
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public string Recommandation { get; set; }
+
+        public string Url { get; set; }
+    }
+}
